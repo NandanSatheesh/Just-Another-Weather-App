b79a7592d66c19f4c2c4eb76a4101186da00c940
Nandan Satheesh <nandan.199776@gmail.com>
2019-08-10 19:33:40 +0530

    Removed API Key

--- app/src/main/java/com/project/weatherapp/network/NetworkUtil.java
+++ app/src/main/java/com/project/weatherapp/network/NetworkUtil.java
@@ -4,5 +4,5 @@ public final class NetworkUtil {
 
   public static final String BASE_URL = "https://api.apixu.com/v1/";
 
-  public static final String API_KEY = "<ADD YOUR API KEY HERE";
+  public static final String API_KEY = "<ADD YOUR API KEY HERE>";
 }
